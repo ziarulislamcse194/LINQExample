36fe0b57add388e2e33867639bc671d12583db45
ziarulislamcse194 <ziarulislamcse194@gmail.com>
2019-07-21 20:51:06 +0600

    Add project files.

---
+++ LINQExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
